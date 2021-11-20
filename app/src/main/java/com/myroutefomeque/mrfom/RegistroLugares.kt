48109d348a1a691a33628bc95d1a3acac4f34742
Gitsjoe <jlopjen@gmail.com>
2021-11-20 15:50:18 -0500

    Registro de lugares creado con objetos

---
+++ app/src/main/java/com/myroutefomeque/mrfom/RegistroLugares.kt
@@ -0,0 +1,11 @@
+package com.myroutefomeque.mrfom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroLugares : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_lugares)
+    }
+}
