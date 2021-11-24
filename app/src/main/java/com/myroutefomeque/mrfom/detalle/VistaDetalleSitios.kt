59187fb7a2eeba5aecee6d1060a02473d0027646
Gitsjoe <jlopjen@gmail.com>
2021-11-23 23:32:03 -0500

    Codificar activity VistaDetalleSitios para que despliegue la informacion que viene del layout ListasitiosTuristicosActivity / Reorganizar objetos dentro del layout de los items, quitar boton ir porqu eno se necesita ya que al dar click a un item nos dirige a la vista detalle.

--- app/src/main/java/com/myroutefomeque/mrfom/detalle/VistaDetalleSitios.kt
+++ app/src/main/java/com/myroutefomeque/mrfom/detalle/VistaDetalleSitios.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.myroutefomeque.mrfom.R
 import com.myroutefomeque.mrfom.databinding.ActivityVistaDetalleSitiosBinding
+import com.myroutefomeque.mrfom.model.SitiosTuristicosItem
 
 class VistaDetalleSitios : AppCompatActivity() {
 
@@ -16,7 +17,14 @@ class VistaDetalleSitios : AppCompatActivity() {
 
         setContentView(detalleBinding.root)
 
-        val nombre = intent.extras?.getString("nombre")
-        detalleBinding.nameTextView.text = nombre
+        val sitiosturisticos : SitiosTuristicosItem = intent.extras?.getSerializable("sitiosturisticos") as SitiosTuristicosItem
+        with(detalleBinding){
+            nameTextView.text = sitiosturisticos.nombre
+            temperatureTextView.text = sitiosturisticos.temperatura.toString()
+            puntajeTextView.text = sitiosturisticos.puntuacion.toString()
+            locationTextView.text = sitiosturisticos.ubicacion
+            descriptionTextView.text = sitiosturisticos.infoGeneral
+            sitioRecTextView.text = sitiosturisticos.sitiosRecomendados
+        }
     }
 }
