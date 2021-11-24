6445118510c50194ff890cb5dccfc586979cbbe4
Gitsjoe <jlopjen@gmail.com>
2021-11-24 13:44:21 -0500

    Copiamos el layout de activity_vista_detalle_sitios del modulo app y lo pegamos en fragment_details del modulo mirutafomeque, traemos los recursos como los strings, los drawables, y corregimos lo que marque en rojo el codigo del layout

---
+++ mirutafomeque/src/main/java/com/myroutefomeque/mirutafomeque/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.myroutefomeque.mirutafomeque.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myroutefomeque.mirutafomeque.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
