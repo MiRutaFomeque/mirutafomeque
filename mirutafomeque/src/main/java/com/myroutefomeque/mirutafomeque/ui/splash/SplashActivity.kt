33133e6d574f66a8171a6dc8aaa448547694ef3b
Gitsjoe <jlopjen@gmail.com>
2021-12-07 13:43:40 -0500

    Activity:splash agregado

---
+++ mirutafomeque/src/main/java/com/myroutefomeque/mirutafomeque/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.myroutefomeque.mirutafomeque.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myroutefomeque.mirutafomeque.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
