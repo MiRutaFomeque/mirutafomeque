a711e0c7d3a22d1749071a0f2109f908b7d3bef0
Gitsjoe <jlopjen@gmail.com>
2021-12-07 14:08:06 -0500

    Splash Screen configurado con exito

--- mirutafomeque/src/main/java/com/myroutefomeque/mirutafomeque/ui/splash/SplashActivity.kt
+++ mirutafomeque/src/main/java/com/myroutefomeque/mirutafomeque/ui/splash/SplashActivity.kt
@@ -1,12 +1,31 @@
 package com.myroutefomeque.mirutafomeque.ui.splash
 
+import android.content.Intent
+import android.view.Window
+import  java.util.*
+import kotlin.concurrent.timerTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import com.myroutefomeque.mirutafomeque.R
+import com.myroutefomeque.mirutafomeque.ui.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_splash)
+
+        val timer = Timer()
+        timer.schedule(timerTask {
+            goToMainActivity()
+        }, 3000
+        )
+    }
+
+    private fun goToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
